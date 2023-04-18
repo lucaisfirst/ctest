a717f1360133f9932c86116c46e51a3eb37c4c12
김하진 <knj9417@gmail.com>
2023-04-18 20:44:29 +0900

    lecture 1~3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ctest
+
+I'm gonna write something about C programming, and this is just exercise note. I will finish this in 2 month.
--- hello.c
+++ hello.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-#define TEXT "고려대에 합격하자!"
 
-int add(int a, int b)
-{
-    return a + b;
-}
+// int main()
+// {
+//     int age = 23;
+//     printf(" \"우리들의 희망을 찾아서 떠나자\n\"I just want to go home\"\n");
+//     printf("저는 %i살 입니다.\n", age);
+//     printf("정답이 %f로 나왔다.", 2.334242);
+// }
+
+// int main()
+// {
+//     float num = 3.14; // double, float: 소수 표현
+//     printf("원주율은 %f입니다.\n", num);
+//     printf("원주율은 %.2f입니다.", num);
+// }
 
 int main()
 {
-    printf("%d\n", add(3, 5));
-    printf(TEXT);
-    return 0;
+    printf("%x", 17);
+    printf("퍼센트는 다음과 같이 표기 합니다. \"%%\"\n");
+    printf("아기의 나이는 %d이고\n제 성적은 전반적으로 %c입니다.\n원주율은 %.2lf입니다.", 4, 'a', 3.14);
 }
