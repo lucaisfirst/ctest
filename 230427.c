82af7d45145d629d3151ddc64c14cb7ba02ad273
김하진 <knj9417@gmail.com>
2023-04-27 22:43:19 +0900

    lecture 3

---
+++ 230427.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int age; // 4byte 정수형 변수 age를 선언한다.
+    double pi;
+    // printf("나이를 입력해주세요: ");
+    // scanf("%d", &age);
+    // printf("원주율은 얼마인가요?: ");
+    // scanf("%lf", &pi);
+    printf("나이와 원주율을 함께 입력해주세요: ");
+    scanf("%d %lf", &age, &pi);
+    printf("나이는: %d, 원주율 %.2lf", age, pi);
+}
